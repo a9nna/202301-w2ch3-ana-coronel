b2f80343405d3afb7beef3ca4ca0949c1af921b9
Ana Coronel <a9nna.coronel@gmail.com>
2023-01-19 07:08:33 +0100

    Refactor the code of the calcularor-pro.js file

--- calculator-pro-js/calculator-pro.js
+++ calculator-pro-js/calculator-pro.js
@@ -1,24 +1,19 @@
 function calculator() {
   let number;
-  let numbers = [];
-  let results = [];
+  const numbers = [];
+  const results = [];
+  const numbersToOperateWith = [];∫
 
-  function isNumber(val) {
+  const isNumber = (val) => {
     if (val !== null && val.includes("/")) {
-      let operation = val;
-      let separation = operation.split("/");
+      const operation = val;
+      const separation = operation.split("/");
       val = separation[0] / separation[1];
     }
-
-    if (!isNaN(val)) {
-      numbers.push(val);
-    } else {
-      alert("The entered data is not a number, please try again");
-    }
-  }
+  };
 
   function decimals(val) {
-    if (val % 1 !== 0) {
+    if (!val % 1 === 0) {
       results.push(val.toFixed(3));
     } else {
       results.push(val);
@@ -28,41 +23,49 @@ function calculator() {
   do {
     number = prompt("Enter a number");
     isNumber(number);
-  } while (number != null);
+  } while (number !== null);
 
   numbers.pop();
 
   if (numbers.length === 1) {
-    let result1 = Math.sqrt(numbers[0]);
+    const result1 = Math.sqrt(numbers[0]);
     decimals(result1);
-    console.log(`The square root of ${numbers[0]} is ${results}`);
-  } else {
-    function multipleOperations() {
-      let add = Number(arguments[0]);
-      let subs = Number(arguments[0]);
-      let mult = Number(arguments[0]);
-      let div = Number(arguments[0]);
-
-      for (let i = 1; i < arguments.length; i++) {
-        add += Number(arguments[i]);
-        subs -= Number(arguments[i]);
-        mult *= Number(arguments[i]);
-        div /= Number(arguments[i]);
-      }
-      decimals(add);
-      decimals(subs);
-      decimals(mult);
-      decimals(div);
-
-      return results;
-    }
-    let allResults = multipleOperations(...numbers);
-
-    console.log(`The result of addition is ${allResults[0]}`);
-    console.log(`The result of substraction is ${allResults[1]}`);
-    console.log(`The result of multiplication is ${allResults[2]}`);
-    console.log(`The result of division is ${allResults[3]}`);
+    return `The square root of ${numbers[0]} is ${results}`;
   }
+
+  const multipleOperations = () => {
+    let add = Number(numbersToOperateWith[0]);
+    let subs = Number(numbersToOperateWith[0]);
+    let mult = Number(numbersToOperateWith[0]);
+    let div = Number(numbersToOperateWith[0]);
+
+    for (let i = 1; i < numbersToOperateWith.length; i++) {
+      add += Number(numbersToOperateWith[i]);
+      subs -= Number(numbersToOperateWith[i]);
+      mult *= Number(numbersToOperateWith[i]);
+      div /= Number(numbersToOperateWith[i]);
+    }
+
+    decimals(add);
+    decimals(subs);
+    decimals(mult);
+    decimals(div);
+
+    return results;
+  };
+
+  if (!isNaN(val)) {
+    numbers.push(val);
+  } else if (isNaN(val)) {
+    alert("The entered data is not a number, please try again");
+  }
+
+  const allResults = multipleOperations(...numbers);
+
+  console.log(`The result of addition is ${allResults[0]}`);
+  console.log(`The result of substraction is ${allResults[1]}`);
+  console.log(`The result of multiplication is ${allResults[2]}`);
+  console.log(`The result of division is ${allResults[3]}`);
 }
 
 calculator();
@@ -76,4 +79,4 @@ do {
   } else {
     calculator();
   }
-} while (anotherOne != null);
+} while (anotherOne !== null);
