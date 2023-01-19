d5496e2edbd5799ab5cab1d44bc7eb958c6dd3f1
Ana Coronel <a9nna.coronel@gmail.com>
2023-01-19 08:26:16 +0100

    Refactor the code of the airlines.js file

--- airlines-js/airlines.js
+++ airlines-js/airlines.js
@@ -1,18 +1,18 @@
 function isdiCodersAirlines() {
-  let welcome = prompt("Write your name");
+  const welcome = prompt("Write your name");
   alert(`Welcome ${welcome}, it is a pleasure you visit ISDI Coders Airlines`);
 
   const flights = [
-    { id: 00, to: "New York", from: "Barcelona", cost: 700, scale: false },
-    { id: 01, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
-    { id: 02, to: "Paris", from: "Barcelona", cost: 210, scale: false },
-    { id: 03, to: "Roma", from: "Barcelona", cost: 150, scale: false },
-    { id: 04, to: "London", from: "Madrid", cost: 200, scale: false },
-    { id: 05, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
-    { id: 06, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
-    { id: 07, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
-    { id: 08, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
-    { id: 09, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
+    { id: 0, to: "New York", from: "Barcelona", cost: 700, scale: false },
+    { id: 1, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
+    { id: 2, to: "Paris", from: "Barcelona", cost: 210, scale: false },
+    { id: 3, to: "Roma", from: "Barcelona", cost: 150, scale: false },
+    { id: 4, to: "London", from: "Madrid", cost: 200, scale: false },
+    { id: 5, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
+    { id: 6, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
+    { id: 7, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
+    { id: 8, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
+    { id: 9, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
   ];
 
   for (let i = 0; i < flights.length; i++) {
@@ -21,6 +21,7 @@ function isdiCodersAirlines() {
     } else {
       flights[i].scale = `it doesn't do a scale`;
     }
+
     console.log(
       `The flight from: ${flights[i].from}, to: ${flights[i].to} costs ${flights[i].cost}€ and ${flights[i].scale}`
     );
@@ -35,20 +36,22 @@ function isdiCodersAirlines() {
   addition /= flights.length;
   console.log(`The average cost of flights is ${addition.toFixed(2)}€`);
 
-  let flightsStopOff = [];
+  const flightsStopOff = [];
 
   for (let i = 0; i < flights.length; i++) {
     if (flights[i].scale === "it does a scale") {
       flightsStopOff.push(flights[i]);
     }
   }
+
   console.log(`${flightsStopOff.length} flights do a scale`);
 
-  let lastFlights = [];
+  const lastFlights = [];
 
   for (let i = 5; i < flights.length; i++) {
     lastFlights.push(flights[i].to);
   }
+
   console.log(`The last five flights are to ${lastFlights}`);
 }
 
